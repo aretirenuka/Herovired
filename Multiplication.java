3de564aa5008ccc8b4b733e032d17d022641e01c
aretirenuka <84584679+aretirenuka@users.noreply.github.com>
2023-04-02 22:37:43 +0530

    Add files via upload

---
+++ Multiplication.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.util.Scanner;
+import java.io.*;
+import java.lang.Math;
+class Multiplication
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int num=5;
+        for(int i=1;i<=10;i++)
+        {
+            System.out.println(num+"x"+i+"="+num*i);
+        }
+    }
+}
