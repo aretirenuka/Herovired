a8b912b929e0c721f857fe52895a58be08fc3ad6
aretirenuka <84584679+aretirenuka@users.noreply.github.com>
2023-04-02 22:39:40 +0530

    Add files via upload

---
+++ Swap.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.util.Scanner;
+import java.io.*;
+import java.lang.Math;
+class Swap
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=63;
+        int b=15;
+        
+        int temp=a;
+        a=b;
+        b=temp; 
+        
+        System.out.println("a:"+ a);
+        System.out.println("b:"+ b);
+    }
+}
