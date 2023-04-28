9cf6ce864e73e7f8c5ed0d62f73c78cb51076aeb
aretirenuka <84584679+aretirenuka@users.noreply.github.com>
2023-04-28 10:15:37 +0530

    Create List.java

---
+++ List.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner in = new Scanner(System.in);
+        int size = in.nextInt();
+        
+        List<Integer> source = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            source.add(in.nextInt());
+        }
+        
+        int queries = in.nextInt();
+        for (int l = 0; l < queries; l++) {
+            String query = in.next();
+            
+            switch (query) {
+                case "Insert":
+                    int index = in.nextInt();
+                    int value = in.nextInt();
+                    source.add(index, value);
+                    break;
+                case "Delete":
+                    source.remove(in.nextInt());
+                    break;
+            }
+        }
+        
+        for (Integer i : source) {
+            System.out.print(i + " ");
+        }
+
+        in.close();
+    }
+}
