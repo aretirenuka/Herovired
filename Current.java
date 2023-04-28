d631ffda8b2ba39856f49af1105e3ad8d2f4db8f
aretirenuka <84584679+aretirenuka@users.noreply.github.com>
2023-04-28 10:49:08 +0530

    Create Current.java

---
+++ Current.java
@@ -0,0 +1,22 @@
+
+import java.util.*;
+import java.text.*;
+
+public class Current {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+        
+     String us,india,china,france;
+   us = NumberFormat.getCurrencyInstance(Locale.US).format(payment);
+   india = NumberFormat.getCurrencyInstance(new Locale("EN","IN")).format(payment);
+   china= NumberFormat.getCurrencyInstance(Locale.CHINA).format(payment);
+   france= NumberFormat.getCurrencyInstance(Locale.FRANCE).format(payment);
+    System.out.println("US: " + us);
+    System.out.println("India: " + india);
+    System.out.println("China: " + china);
+    System.out.println("France: " + france);
+}
+}
